cb6ecedcc1298308269a8fc28dff3c40de601c8a
Aqeel Mozumder <aqeelmozumder@hotmail.com>
2021-03-12 17:08:39 -0800

    commiting from omen

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 const DIST_DIR = path.join(__dirname, '/src');
-const HTML_FILE = path.join(DIST_DIR, 'index.html');
+const HTML_FILE = path.join(DIST_DIR, 'Index.html');
 
 app.use(express.static(DIST_DIR));
 app.get('*', (req, res) => {
